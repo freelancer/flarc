65561cc414c9ca0852230feeadc5a91325adee60
Joshua Spence <josh@freelancer.com>
2019-05-07 09:44:38 +1000

    Check that the vendor directory exists before attempting to register the Composer autoloader
    Summary:
    Ref T87915. `PhutilBootloader:executeInclude` assumes that an `include` was unsuccessful if [[https://www.php.net/manual/en/function.error-get-last.php | `error_get_last`]] reports than an error was triggered. Since PHP's [[https://www.php.net/manual/en/language.operators.errorcontrol.php | error control opreator]] doesn't seem to prevent errors from being returned from `error_get_last()`, call [[https://www.php.net/manual/en/function.error-clear-last.php | `error_clear_last`]] so that `PhutilBootloader` knows that everything is okay. Depends on D125570.
    
    ```name="arc linters"
     Exception
    Error while loading file "/home/josh/workspace/freelancer/flarc/src/__phutil_library_init__.php": include_once(): Failed opening '/home/josh/workspace/freelancer/flarc/src/../vendor/autoload.php' for inclusion (include_path='/home/josh/workspace/personal/libphutil/externals/includes:/home/josh/workspace/personal:.:/usr/share/php:/home/josh/workspace/personal/arcanist/externals/includes')
    (Run with `--trace` for a full exception trace.)
    ```
    
    Test Plan: Wiped out the `vendor/` directory and was able to run `arc linters`.
    
    Reviewers: tpham, szhu
    
    Reviewed By: tpham, szhu
    
    Maniphest Tasks: T87915
    
    Differential Revision: https://phabricator.tools.flnltd.com/D125630

--- src/__phutil_library_init__.php
+++ src/__phutil_library_init__.php
@@ -5,4 +5,6 @@ phutil_register_library('flarc', __FILE__);
 // Register the Composer autoloader.
 //
 // NOTE: Composer dependencies are currently only required for unit testing.
-@include_once __DIR__.'/../vendor/autoload.php';
+if (Filesystem::pathExists(__DIR__.'/../vendor/autoload.php')) {
+  include_once __DIR__.'/../vendor/autoload.php';
+}
