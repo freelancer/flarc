00cd85ba2b9139771f6256a789c745ccd2972ae3
Joshua Spence <josh@freelancer.com>
2019-05-11 14:45:15 +1000

    Add a TODO comment

--- src/lint/linter/__tests__/ArcanistTerraformFmtLinterTestCase.php
+++ src/lint/linter/__tests__/ArcanistTerraformFmtLinterTestCase.php
@@ -3,6 +3,9 @@
 final class ArcanistTerraformFmtLinterTestCase
   extends ArcanistExternalLinterTestCase {
 
+  /**
+   * TODO: These tests fail on Terraform 0.12.
+   */
   public function testLinter(): void {
     $this->executeTestsInDirectory(__DIR__.'/terraform-fmt/');
   }
