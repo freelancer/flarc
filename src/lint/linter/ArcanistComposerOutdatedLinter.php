54f733d6762eb82790d79e0f195f8ed7b1b57895
Sijun Zhu <szhu@freelancer.com>
2019-05-21 15:45:09 +1000

    Add `ArcanistComposerOutdatedLinter`
    Summary: Ref T21888, Add `composer outdated --direct` to arcanist.
    
    Test Plan:
    1. add the following to `.arclint`'s linter
    ```
            "composer-outdated": {
                "type": "composer-outdated",
                "include": "(composer.json$)"
            },
    ```
    2. modify `composer.json` (e.g. add an empty line)
    3. `arc lint`
    
    RESULT:
    ```
       Advice  (Composer Package Outdated) update-possible
        Package [apache/thrift] is outdated, consider update it from [0.11.0] to
        [v0.12.0].
        Caution: Update might be backward incompatible
        Package Description: Apache Thrift RPC system
    
       Advice  (Composer Package Outdated) update-possible
        Package [phpmailer/phpmailer] is outdated, consider update it from
        [v5.2.27] to [v6.0.7].
        Caution: Update might be backward incompatible
        Package Description: PHPMailer is a full-featured email creation and
        transfer class for PHP
    
       Advice  (Composer Package Outdated) update-possible
        Package [willdurand/geocoder] is outdated, consider update it from
        [2.8.2] to [4.2.1].
        Caution: Update might be backward incompatible
        Package Description: The almost missing Geocoder PHP 5.3 library.
    
       Advice  (Composer Package Outdated) semver-safe-update
        Package [seostats/seostats] is outdated, consider update it from
        [dev-master 03cf5f9] to [dev-master bd3117f].
        Update is minor and safe
        Package Description: SEOstats is a powerful open source PHP library to
        request a bunch of SEO relevant metrics for any website.
    
       Advice  (Composer Package Outdated) update-possible
        Package [sentry/sentry] is outdated, consider update it from [0.11.0] to
        [2.0.1].
        Caution: Update might be backward incompatible
        Package Description: A PHP client for Sentry (http://getsentry.com)
    
    .
    .
    .
    .
    ```
    
    Reviewers: josh, O8 Phabricator
    
    Reviewed By: josh, O8 Phabricator
    
    Maniphest Tasks: T21888
    
    Differential Revision: https://phabricator.tools.flnltd.com/D126539

---
+++ src/lint/linter/ArcanistComposerOutdatedLinter.php
@@ -0,0 +1,148 @@
+<?php
+
+/**
+ * TODO: Can we merge this into `ArcanistComposerLinter`?
+ */
+final class ArcanistComposerOutdatedLinter extends ArcanistExternalLinter {
+
+  public function getInfoName(): string {
+    return pht('Composer Outdated');
+  }
+
+  public function getInfoUrl(): string {
+    return 'https://getcomposer.org';
+  }
+
+  public function getInfoDescription(): string {
+    return pht('Checks whether Composer packages are up-to-date.');
+  }
+
+  public function getLinterName(): string {
+    return 'Composer Outdated';
+  }
+
+  public function getLinterConfigurationName(): string {
+    return 'composer-outdated';
+  }
+
+  public function getDefaultBinary(): string {
+    return 'composer';
+  }
+
+  public function getInstallInstructions(): string {
+    return pht(
+      'See %s for installation instructions.',
+      'https://getcomposer.org');
+  }
+
+  public function getVersion(): ?string {
+    list($stdout) = execx('%C --version', $this->getExecutableCommand());
+
+    $matches = null;
+    $regex = '/^Composer version (?P<version>\d+\.\d+\.\d+)\b/';
+
+    if (!preg_match($regex, $stdout, $matches)) {
+      return null;
+    }
+
+    return $matches['version'];
+  }
+
+  protected function getMandatoryFlags(): array {
+    return [
+      'outdated',
+      '--direct',
+      '--format=json',
+    ];
+  }
+
+  public function shouldExpectCommandErrors(): bool {
+    return false;
+  }
+
+  /**
+   * composer outdated command should not take in any path argument
+   *
+   * @throws PhutilProxyException
+   */
+  protected function buildFutures(array $paths) {
+    $bin = csprintf('%C %Ls', $this->getExecutableCommand(), $this->getCommandFlags());
+    $futures = [];
+
+    foreach ($paths as $path) {
+      $disk_path = $this->getEngine()->getFilePathOnDisk($path);
+
+      $futures[$path] = (new ExecFuture('%C', $bin))
+        ->setEnv([
+          'COMPOSER' => $disk_path,
+          'HOME'     => getenv('HOME'),
+          'PATH'     => getenv('PATH'),
+        ])
+        ->setCWD($this->getProjectRoot());
+    }
+
+    return $futures;
+  }
+
+  protected function parseLinterOutput($path, $err, $stdout, $stderr) {
+    try {
+      $packages = phutil_json_decode($stdout);
+    } catch (PhutilJSONParserException $ex) {
+      throw new PhutilProxyException(
+        pht(
+          "Failed to parse `%s` output.\n\nSTDOUT\n%s",
+          'composer',
+          $stdout),
+        $ex);
+    }
+
+    return array_map(
+      function (array $package) use ($path): ArcanistLintMessage {
+        return (new ArcanistLintMessage())
+          ->setPath($path)
+          ->setCode($package['latest-status'])
+          ->setSeverity(ArcanistLintSeverity::SEVERITY_ADVICE)
+          ->setName(pht('Composer Package Outdated'))
+          ->setDescription($this->formatMessage($package))
+          ->setBypassChangedLineFiltering(true);
+      },
+      $packages['installed']);
+
+    $messages = [];
+    foreach ($packages['installed'] as $package) {
+      $messages[] = (new ArcanistLintMessage())
+        ->setCode(pht('Composer Package Outdated'))
+        ->setName(pht('%s', $package['latest-status']))
+        ->setDescription(pht('%s', $this->formatMessage($package)))
+        ->setSeverity(ArcanistLintSeverity::SEVERITY_ADVICE)
+        ->setBypassChangedLineFiltering(true);
+    }
+
+    return $messages;
+  }
+
+  private function formatMessage(array $package): string {
+    if (isset($package['warning'])) {
+      return $package['warning'];
+    }
+
+    $formatted_str = pht(
+      'Package `%s` is outdated, consider updating it from `%s` to `%s`.',
+      $package['name'],
+      $package['version'],
+      $package['latest']);
+
+    switch ($package['latest-status']) {
+      case 'semver-safe-update':
+        $formatted_str .= ' '.pht('Update is minor and safe.');
+        break;
+
+      case 'update-possible':
+        $formatted_str .= ' '.pht('Caution: Update might be backward incompatible.');
+        break;
+    }
+
+    return $formatted_str;
+  }
+
+}
