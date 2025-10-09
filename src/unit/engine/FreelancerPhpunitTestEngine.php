cc37efa98ce8b59f28ab4e701832c087a2300c90
Gergo Magyar <gmagyar@freelancer.com>
2025-10-09 14:12:38 +0100

    [rFLARC] Make FreelancerPhpunitTestEngine put reports in gaf/reports
    Summary: Report will be output in the `reports` folder after this change.
    
    Test Plan:
    [] coverage reports are save under `./reports./[reportDirectory]/[filename].xml`
    
    {F4570602 width=100%}
    
    [] coverage report (xmls) have proper content
    
    {F4569605 width=100%}
    
    [] coverage report properly loads in phpstorm
    
    |Covered function|Non-Covered Function
    |---|---
    |{F4569608 width=100%}|{F4569607 width=100%}
    
    [] coverage report appears in cli
    
    ```
    $ arc unit -- test/unit/src/Education/Academy/CourseHandlerTest.php
     Source Tests
    .......
    
    COVERAGE REPORT
          0%     test/unit/src/Education/Academy/CourseHandlerTest.php
         27%     src/Education/Academy/CourseHandler.php
    ```
    
    [] consequently arcanist is able to parse the test results
    
    {F4569618 width=100%}
    
    [] Set `JENKINS_HOME` to `true` and verify the output `junit.xml`
    
    {F4569626 width=100%}
    
    This is what we defined in the `./support/build/qa-box/PHPTest.Jenkinsfile` and `./support/build/PHP.Jenkinsfile` to generate the merged `junit.xml`
    
    Reviewers: O43 Developer Experience, #developer_experience_team, #ai_infra, tbolton, O8 Phabricator, gread
    
    Reviewed By: O43 Developer Experience, #developer_experience_team, #ai_infra, tbolton, O8 Phabricator, gread
    
    Subscribers: gmagyar, tbolton
    
    Maniphest Tasks: T314632
    
    Differential Revision: https://phabricator.tools.flnltd.com/D215135

--- src/unit/engine/FreelancerPhpunitTestEngine.php
+++ src/unit/engine/FreelancerPhpunitTestEngine.php
@@ -67,6 +67,12 @@ final class FreelancerPhpunitTestEngine
       $this->getConfigPath('unit.phpunit.source-directory'));
     $this->setTestDirectory(
       $this->getConfigPath('unit.phpunit.test-directory'));
+    $this->setTestType(
+      $this->getConfigValue('unit.phpunit.test-type'));
+    $this->setReportDirectory(
+      $this->getConfigValue('unit.phpunit.reports'));
+
+
 
     if ($this->getRunAllTests()) {
       $this->setPaths([$this->testDirectory]);
@@ -88,8 +94,11 @@ final class FreelancerPhpunitTestEngine
         if (!Filesystem::pathExists($test_path)) {
           continue;
         }
+        $test_name = basename($test_path, '.php');
+        $output_files = $this->generateOutputFiles(
+          $enable_coverage,
+          $test_name);
 
-        $output_files = $this->generateOutputFiles($enable_coverage);
         $args = $this->getBinaryArgs(
           $config,
           $enable_coverage,
@@ -172,8 +181,6 @@ final class FreelancerPhpunitTestEngine
   }
 
 /* -(  Utility  )------------------------------------------------------------ */
-
-
   /**
    * Retrieve a path to an executable from the `.arcconfig` file.
    *
@@ -239,14 +246,5 @@ final class FreelancerPhpunitTestEngine
         $enable_coverage,
         $junit_output,
         $clover_output));
-  }
-
-  protected function generateOutputFiles(
-    bool $enable_coverage,
-    ?string $file_name = null): array {
-    return [
-      'clover' => $enable_coverage ? new TempFile() : null,
-      'junit' => new TempFile(),
-    ];
   }
 }
